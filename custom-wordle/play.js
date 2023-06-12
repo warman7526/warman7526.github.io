89f0313d204b9369c264c163d1088d37c939e0d1
warman7526 <warman7526@bradfordgrammar.com>
2023-06-12 18:23:37 +0100

    added theme structure
    theme css+js files, added taytay theme

--- custom-wordle/play.js
+++ custom-wordle/play.js
@@ -149,28 +149,3 @@ function choice(a) {
 
 createGrid(word.length, 6)
 
-function getDate() {
-    const months = "JanFebMarAprMayJunJulAugSepOctNovDec"
-    const d = new Date();
-    var day = d.getDate().toString();
-    var month = months.substring(d.getMonth() * 3, (d.getMonth() + 1) * 3)
-    return (day.length == 1 ? "0" : "") + day + month;
-}
-function setTheme(d) {
-    var specials = {
-        "27Jun": "fireworks",
-        "24Dec": "xmas",
-        "25Dec": "xmas",
-        "26Dec": "xmas",
-        "01Jan": "fireworks",
-        "31Dec": "fireworks"
-    }
-    if (!Object.keys(specials).includes(d)) {
-        document.querySelector("body").id = "";
-        document.querySelector("body").setAttribute("i", randint(1, 8)); document.querySelector("body").innerHTML += "<span class='bgcredits' onclick='window.open(`https://www.youtube.com/@hypermind15`,`_blank`)'>Background © Hypermind 2023</span>"
-    } else {
-        document.querySelector("body").id = specials[d];
-
-    }
-}
-setTheme(getDate())
--- custom-wordle/script.js
+++ custom-wordle/script.js
@@ -37,21 +37,3 @@ function getDate() {
     var month = months.substring(d.getMonth() * 3, (d.getMonth() + 1) * 3)
     return (day.length == 1 ? "0" : "") + day + month;
 }
-function setTheme(d) {
-    var specials = {
-        "27Jun": "fireworks",
-        "24Dec": "xmas",
-        "25Dec": "xmas",
-        "26Dec": "xmas",
-        "01Jan": "fireworks",
-        "31Dec": "fireworks"
-    }
-    if (!Object.keys(specials).includes(d)) {
-        document.querySelector("body").id = "";
-        document.querySelector("body").setAttribute("i", randint(1, 8)); document.querySelector("body").innerHTML += "<span class='bgcredits' onclick='window.open(`https://www.youtube.com/@hypermind15`,`_blank`)'>Background © Hypermind 2023</span>"
-    } else {
-        document.querySelector("body").id = specials[d];
-
-    }
-}
-setTheme(getDate())
