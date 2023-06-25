f1230064ae23fecc63bfbb406223d508b0b205ae
warman7526 <warman7526@bradfordgrammar.com>
2023-06-25 16:40:01 +0100

    oops
    oopslol

--- inject.js
+++ inject.js
@@ -4,4 +4,4 @@ const body = document.querySelector("body");
 
 // add google adsense code
 const adsenseCode = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6678138025809339" crossorigin="anonymous"></script>`
-head.innerHTML.concat(adsenseCode);
+head.innerHTML += adsenseCode;
